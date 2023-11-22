0c95d1706c7a309cb22f361d8266d556d48d770d
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-11-22 15:50:40 +0000

    New db created

--- Day2/Pt9_10DB/connect.py
+++ Day2/Pt9_10DB/connect.py
@@ -1,4 +1,7 @@
 import sqlite3 as sql #import sqlite3 mode and assigned as alias'sql'
 
+#from sqlite3 import connect 
 
-from sqlite3 import connect 
+#use sqlite (sql) module to create and/or connect to a database
+dbCon = sql.connect('Day2/Pt9_10DB/dfe2.db')
+
