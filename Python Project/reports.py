ee926328b0d2f9b8c519400cfed4758275fdba05
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-11-27 11:47:21 +0000

    Create reports.py

---
+++ Python Project/reports.py
@@ -0,0 +1,15 @@
+from connect import dbCursor, dbCon
+ 
+# create subroutine
+def reports():
+    #dbCursor.execute("SELECT title FROM tblFilms ORDER BY title DESC ")
+    #dbCursor.execute("SELECT * FROM tblFilms WHERE Genre = 'Crime' OR Genre ='Action' ")
+    #dbCursor.execute("SELECT * FROM tblFilms WHERE Genre LIKE 'r%' ")
+    dbCursor.execute("SELECT * FROM tblFilms WHERE Genre NOT LIKE 'r%' ")
+    #dbCursor.execute("SELECT * FROM tblFilms WHERE genre LIKE '%r' ")
+    reportsData = dbCursor.fetchall()
+ 
+    for records in reportsData:
+        print(records)
+if __name__ == "__main__":
+    reports()
