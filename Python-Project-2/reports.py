34ba5afa0a97a71b6107d946984b8fd437734475
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-11-28 11:30:50 +0000

    Create reports.py

---
+++ Python-Project-2/reports.py
@@ -0,0 +1,15 @@
+from connect import dbCursor, dbCon
+ 
+# create subroutine
+def reports1():
+    #dbCursor.execute("SELECT title FROM recipeBook ORDER BY title DESC ")
+    #dbCursor.execute("SELECT * FROM recipeBook WHERE recipeName = 'Rice pudding' OR 'Chocolate Cake' ")
+    #dbCursor.execute("SELECT * FROM trecipeBook  WHERE recipeName LIKE 'r%' ")
+    dbCursor.execute("SELECT * FROM recipeBook  WHERE recipeName NOT LIKE 'r%' ")
+    #dbCursor.execute("SELECT * FROM recipeBook  WHERE recipeName LIKE '%r' ")
+    reportsData = dbCursor.fetchall()
+ 
+    for records in reportsData:
+        print(records)
+if __name__ == "__main__":
+    reports1()
