50e323b23f6b463486af48a33d063f36e925f10b
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-11-28 11:26:46 +0000

    Update file created

---
+++ Python-Project-2/updaterecipes.py
@@ -0,0 +1,33 @@
+from connect import dbCursor, dbCon
+
+def update_recipeBook():
+    # Use primary key to update a record
+    idField = input("Enter the recipe ID of the record you want to update: ")
+
+    # Field to update 
+    fieldName = input("Enter RecipeName or Ingredients or Instructions as field name: ").title()
+
+    # Value for the field
+    fieldValue = input(f"Enter the value for {fieldName} field: ")
+
+      # (1, 'Chocolate Cake', 'Flour, Sugar, Cocoa Powder, Eggs, Milk', '1. Mix dry ingredients. 2. Add wet ingredients. 3. Bake at 350°F.')
+
+   # "method 1"
+    fieldValue = fieldValue
+    
+    # "method 2"
+    # tuple(fieldValue)
+    # print(fieldvalue)
+    # num = "2"
+    # int(mum)
+
+    # Update a record 
+    query = f"UPDATE recipeBook SET {fieldName} = ? WHERE recipeID = ?"
+    dbCursor.execute(query, (fieldValue, idField))
+
+    dbCon.commit()
+
+    print(f"Record {idField} updated ")
+
+if __name__ == "__main__":
+    update_recipeBook()
