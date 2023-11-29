53936c33818adde3682d0582232666ebcd590973
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-11-29 09:17:21 +0000

    changes to add file

--- Python-Project-2/addrecipes.py
+++ Python-Project-2/addrecipes.py
@@ -1,21 +1,27 @@
 from connect import dbCursor, dbCon
-# create a subroutine
+
 def insert_recipeBook():
-    # RecipeId is auto increment field and not data input is required
- 
-    # ask user for input for Recipe Name, Ingredients, Instructions
-    recipeName = input("Enter recipe name: ")
+    # RecipeId is an auto-increment field, and no data input is required
+
+    # Ask the user for input for Recipe Name, Ingredients, Instructions
+    while True:
+        recipeName = input("Enter recipe name: ")
+        if not recipeName.isnumeric():  # Check if the input contains only text
+            break
+        else:
+            print("Invalid input. Please enter a valid recipe name (text only).")
+
     Ingredients = input("Enter recipe ingredients: ")
     Instructions = input("Enter recipe instructions: ")
-   
- 
-    #insert data into the recipe book table
-    dbCursor.execute("INSERT INTO recipeBook(recipeName, Ingredients, Instructions) VALUES(?,?,?)",(recipeName, Ingredients, Instructions ))
-   
+
+    # Insert data into the recipe book table
+    dbCursor.execute("INSERT INTO recipeBook(RecipeName, Ingredients, Instructions) VALUES(?,?,?)", (recipeName, Ingredients, Instructions))
+
     # Commit the change
     dbCon.commit()
- 
-    # testing
+
+    # Testing
     print(f"{recipeName} inserted into recipeBook table")
+
 if __name__ == "__main__":
     insert_recipeBook()
