c055c86b23f5111a1cdc2efb9acf91d5cc460cbd
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-11-29 14:50:46 +0000

    changes to files

--- Python-Project-2/updaterecipes.py
+++ Python-Project-2/updaterecipes.py
@@ -1,30 +1,24 @@
 from connect import dbCursor, dbCon
 
 def update_recipeBook():
-    #Use primary key to update a record
+    # Use primary key to update a record
     while True:
-        idField = input("Enter the recipe ID of the record you want to update: ")
+        idField = input("Enter the recipe ID of the recipe you want to update: ")
         if idField.isdigit():  # Check if the input is a valid number
             break
         else:
             print("Invalid input. Recipe ID should be a number.")
 
-    # Field to update 
-    fieldName = input("Enter RecipeName or Ingredients or Instructions as field name: ").title()
+    allowed_field_names = ["RecipeName", "Ingredients", "Instructions"]
+    while True:
+        fieldName = input("Enter RecipeName, Ingredients, or Instructions as field name: ").title()
+        if fieldName.lower() in map(str.lower, allowed_field_names):
+            break
+        else:
+            print("Invalid input. Please enter one of the stated field names.")
 
     # Value for the field
     fieldValue = input(f"Enter the value for {fieldName} field: ")
-
-      # (1, 'Chocolate Cake', 'Flour, Sugar, Cocoa Powder, Eggs, Milk', '1. Mix dry ingredients. 2. Add wet ingredients. 3. Bake at 350°F.')
-
-   # "method 1"
-    fieldValue = fieldValue
-    
-    # "method 2"
-    # tuple(fieldValue)
-    # print(fieldvalue)
-    # num = "2"
-    # int(mum)
 
     # Update a record 
     query = f"UPDATE recipeBook SET {fieldName} = ? WHERE recipeID = ?"
@@ -36,6 +30,8 @@ def update_recipeBook():
 
 if __name__ == "__main__":
     update_recipeBook()
+
+
     
 
     
