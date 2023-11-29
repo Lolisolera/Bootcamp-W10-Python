217029fee3b788ce1b17bfccb83c60b52f4a922a
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-11-29 13:53:47 +0000

    changes to files

--- Python Project/addfilms.py
+++ Python Project/addfilms.py
@@ -4,11 +4,31 @@ def insert_tblFilms():
     # FilmId is auto increment field and not data input is required
  
     # ask user for input for Title, Rating, Genre
-    filmTitle = input("Enter film title: ")
+    while True:
+        filmTitle = input("Enter film title: ")
+        if not filmTitle.isnumeric():  # Check if the input contains only text
+            break
+        else:
+            print("Invalid input. Please enter a valid film title(text only).")
+
     filmYearReleased = input("Enter film year released: ")
-    filmRating = input("Enter film rating: ")
+
+    while True:
+       filmRating = input("Enter film rating: ")
+       if not filmRating.isnumeric():  # Check if the input contains only text
+         break
+       else:
+            print("Invalid input. Please enter a valid film rating(text only).")
+
     filmDuration = input("Enter film duration: ")
-    filmGenre = input("Enter film genre: ")
+
+
+    while True:
+      filmGenre = input("Enter film genre: ")
+      if not filmGenre.isnumeric():  # Check if the input contains only text
+         break
+      else:
+            print("Invalid input. Please enter a valid film genre(text only).")
  
     #insert data into the films table
     dbCursor.execute("INSERT INTO tblFilms(Title, YearReleased, Rating, Duration, Genre) VALUES(?,?,?,?,?)",(filmTitle, filmYearReleased, filmRating, filmDuration, filmGenre))
@@ -17,10 +37,11 @@ def insert_tblFilms():
     dbCon.commit()
  
     # testing
-    print(f"{filmTitle} inserted into songs table")
+    print(f"{filmTitle} inserted into FilmFlix table")
 if __name__ == "__main__":
     insert_tblFilms()
 
 
 
- 
+  
+   
--- Python Project/updatefilms.py
+++ Python Project/updatefilms.py
@@ -2,24 +2,23 @@ from connect1 import dbCursor, dbCon
 
 def update_tblFilms():
     # Use primary key to update a record
-    idField = input("Enter the film ID of the record you want to update: ")
+    while True:
+        idField = input("Enter the film ID of the record you want to update: ")
+        if idField.isdigit():  # Check if the input is a valid number
+            break
+        else:
+            print("Invalid input. Film ID should be a number.")
 
-    # Field to update 
-    fieldName = input("Enter title or year released or rating or duration or genre as field name: ").title()
+    allowed_field_names = ["title", "year released", "rating", "duration", "genre"]
+    while True:
+        fieldName = input("Enter title, year released, rating, duration, or genre as field name: ").lower()
+        if fieldName in allowed_field_names:
+            break
+        else:
+            print("Invalid input. Please enter one of the stated field names.")
 
     # Value for the field
     fieldValue = input(f"Enter the value for {fieldName} field: ")
-
-      # (1, 'Lost', '1999', 'PG', '90', 'Adventure')
-
-   # "method 1"
-    fieldValue = fieldValue
-    
-    # "method 2"
-    # tuple(fieldValue)
-    # print(fieldvalue)
-    # num = "2"
-    # int(mum)
 
     # Update a record 
     query = f"UPDATE tblFilms SET {fieldName} = ? WHERE filmID = ?"
@@ -31,3 +30,4 @@ def update_tblFilms():
 
 if __name__ == "__main__":
     update_tblFilms()
+
