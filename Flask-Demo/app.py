34625239bb156d18cb91a2b0db8605b6b24c396f
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-11-30 12:09:11 +0000

    Test html file created

--- Flask-Demo/app.py
+++ Flask-Demo/app.py
@@ -3,4 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def base():
+    return render_template("base.html")
+
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
 
