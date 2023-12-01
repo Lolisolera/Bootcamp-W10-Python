2e68ce0e780a36577b6b348b7e3ed43964a09c72
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-12-01 16:04:31 +0000

    New html files created for Flask

--- Flask-Demo/app.py
+++ Flask-Demo/app.py
@@ -4,13 +4,13 @@ from flask import Flask, render_template
 app = Flask(__name__,static_url_path="/static")
 
 @app.route("/")
-def base():
-    return render_template("base.html")
+def home():
+    return render_template("home.html")
 
 
-@app.route("/test")
-def test():
-    return render_template("test.html")
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 @app.route("/about")
 def about():
