687b6ccb8be48f45c5550cd3a4f32fad289e227f
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-12-05 16:24:11 +0000

    Update app.py

--- Flask-API/app.py
+++ Flask-API/app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__,static_url_path="/static")
 
 import os
 
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///filmflix.db'
+db = SQLAlchemy(app)
+api = Api(app)
+
+
+db = SQLAlchemy()
+
+class Film(db.Model):
+    filmID = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    duration = db.Column(db.Integer)
+    genre = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f"<Film {self.filmID}: {self.title}>"
+
+
+
+# Your existing routes for rendering HTML pages
 @app.route("/")
 def base():
     return render_template("base.html")
@@ -40,3 +65,45 @@ def read():
     return render_template("read.html")
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
+
+
+
+# Create your API resources
+class FilmResource(Resource):
+    def get(self, film_id):
+        film = Film.query.get_or_404(film_id)
+        # You can customize the response format as needed
+        return {'filmID': film.filmID, 'title': film.title, 'year': film.year, 'rating': film.rating,
+                'duration': film.duration, 'genre': film.genre}
+
+    def delete(self, film_id):
+        film = Film.query.get_or_404(film_id)
+        db.session.delete(film)
+        db.session.commit()
+        return '', 204
+
+class FilmListResource(Resource):
+    def get(self):
+        films = Film.query.all()
+        return [{'filmID': film.filmID, 'title': film.title, 'year': film.year, 'rating': film.rating,
+                 'duration': film.duration, 'genre': film.genre} for film in films]
+
+    def post(self):
+        data = request.get_json()
+        new_film = Film(title=data.get('title'), year=data.get('year'), rating=data.get('rating'),
+                        duration=data.get('duration'), genre=data.get('genre'))
+        db.session.add(new_film)
+        db.session.commit()
+        return {'filmID': new_film.filmID, 'title': new_film.title, 'year': new_film.year,
+                'rating': new_film.rating, 'duration': new_film.duration, 'genre': new_film.genre}, 201
+
+api.add_resource(FilmListResource, '/films')
+api.add_resource(FilmResource, '/films/<int:film_id>')
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
+
+
+
+
