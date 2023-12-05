8fc93aecc15cd06a023a718ee1bdcfb409cf6eba
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-12-05 11:14:45 +0000

    Update app.py

--- Flask-API(FilmFlix)/app.py
+++ Flask-API(FilmFlix)/app.py
@@ -3,6 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__,static_url_path="/static")
 
 
+
+
+
+
+
+
+
+
+
 @app.route("/")
 def base():
     return render_template("base.html")
